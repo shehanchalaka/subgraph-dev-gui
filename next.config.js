e9152021c72e6ce049d469c2a483744e94674453
Chalaka <shehanchalakafdo@gmail.com>
2022-06-25 14:43:07 +0800

    docker updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  output: 'standalone',
+  experimental: {
+    outputStandalone: true
+  }
 }
-
-module.exports = nextConfig
